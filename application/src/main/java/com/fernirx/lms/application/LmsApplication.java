643de54f9a93d9319664b566f8c5ca7fa4037b48
Truong Ngoc Minh <truongocminh204@gmail.com>
2025-08-14 20:43:27 +0700

    feat(user): implement basic GET endpoints for user module
    - Add GET /users endpoint to retrieve all users
    - Add GET /users/{id} endpoint to retrieve specific user
    - Implement UserDTO for safe data transfer (excluding password)
    - Set up MapStruct mapper for entity-DTO conversion

--- application/src/main/java/com/fernirx/lms/application/LmsApplication.java
+++ application/src/main/java/com/fernirx/lms/application/LmsApplication.java
@@ -2,13 +2,18 @@ package com.fernirx.lms.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication(scanBasePackages = "com.fernirx.lms")
+@SpringBootApplication(scanBasePackages = {"com.fernirx.lms"})
+@EnableJpaRepositories(basePackages = "com.fernirx.lms")
+@EntityScan(basePackages = "com.fernirx.lms")
 @ConfigurationPropertiesScan("com.fernirx.lms")
 public class LmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LmsApplication.class, args);
     }
+
 }
