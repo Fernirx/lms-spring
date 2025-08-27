f202f4753dc9ecc4bae2325db5947f47ef9df261
Truong Ngoc Minh <truongocminh204@gmail.com>
2025-08-27 09:46:12 +0700

    feat(user-mapper): support mapping `Role` entity to `RoleResponseDTO`
    - Added mapping method `toDto(Role role)` to `UserMapper`.
    - Updated `UserResponseDTO` to replace `roleId` with `RoleResponseDTO`.

--- user/src/main/java/com/fernirx/lms/user/mapper/UserMapper.java
+++ user/src/main/java/com/fernirx/lms/user/mapper/UserMapper.java
@@ -2,7 +2,9 @@ package com.fernirx.lms.user.mapper;
 
 
 import com.fernirx.lms.user.dto.request.UserRequestDTO;
+import com.fernirx.lms.user.dto.response.RoleResponseDTO;
 import com.fernirx.lms.user.dto.response.UserResponseDTO;
+import com.fernirx.lms.user.entity.Role;
 import com.fernirx.lms.user.entity.User;
 import org.mapstruct.Mapper;
 
@@ -10,7 +12,11 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+//  User
     User toEntity(UserRequestDTO user);
     UserResponseDTO toDto(User user);
     List<UserResponseDTO> toListDto(List<User> user);
+//  Role
+    RoleResponseDTO toDto(Role role);
+
 }
