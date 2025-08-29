c01615d082b9f2b165fcc7c63c0671bd015270f3
Truong Ngoc Minh <truongocminh204@gmail.com>
2025-08-29 09:47:38 +0700

    feat(controller): add StandardResponseDoc annotations to UserController methods
    Added @StandardResponseDoc annotations to all user-related endpoints in the UserController. Now API documentation includes descriptions, providing better clarity and context for each endpoint.
    
    - Annotated getAllUser() with `Get all user`
    - Annotated getUserById() with `Get a user` and description `Get a user by ID from Lms System`
    - Annotated createUser() with `Create a user` and description `Create a new user in the Lms System with Username, Password, RoleId`
    - Annotated deleteUser() with `Delete a user` and description `Delete a user by ID from Lms System`
    
    No breaking changes introduced.

--- user/src/main/java/com/fernirx/lms/user/controller/UserController.java
+++ user/src/main/java/com/fernirx/lms/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.fernirx.lms.user.controller;
 
+import com.fernirx.lms.common.annotations.docs.StandardResponseDoc;
 import com.fernirx.lms.common.constants.ApiConstants;
 import com.fernirx.lms.common.constants.MessageConstants;
 import com.fernirx.lms.common.dtos.responses.SuccessResponse;
@@ -22,6 +23,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
+    @StandardResponseDoc(value="Get all user")
     public ResponseEntity<SuccessResponse<List<UserResponseDTO>>> getAllUser() {
         List<UserResponseDTO> users = userService.getActiveUsers();
         return ResponseEntity.ok(
@@ -30,18 +32,23 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @StandardResponseDoc(value="Get a user",description = "Get a user by ID from Lms System")
     private ResponseEntity<SuccessResponse<UserResponseDTO>> getUserById(@PathVariable int id) {
         UserResponseDTO user = userService.getUserById(id);
         return ResponseEntity.ok(SuccessResponse.of("User retrieved successfully",user));
     }
 
     @PostMapping
+    @StandardResponseDoc(
+            value="Create a user",
+            description = "Create a new user in the Lms System with Username, Password, RoleId")
     public ResponseEntity<SuccessResponse<UserResponseDTO>> createUser(@Valid @RequestBody UserRequestDTO user) {
         UserResponseDTO userResponse = userService.createUser(user);
         return ResponseEntity.ok(SuccessResponse.of(MessageConstants.SUCCESS_CREATE,userResponse));
     }
 
     @DeleteMapping("/{id}")
+    @StandardResponseDoc(value="Delete a user",description = "Delete a user by ID from Lms System")
     public ResponseEntity<SuccessResponse<Void>> deleteUser(@PathVariable int id) {
         userService.softDeleteUser(id);
         return ResponseEntity.ok(SuccessResponse.of(MessageConstants.SUCCESS_DELETE));
