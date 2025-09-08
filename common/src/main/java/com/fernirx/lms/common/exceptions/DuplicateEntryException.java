a81a67d7b93085cfcf6c849b179c87fab11e78f2
Fernirx <anhtuhungdeveloper@gmail.com>
2025-09-09 00:49:18 +0700

    feat: enhance business exceptions and handlers
    - Add field-level details to ResourceNotFoundException
    - Add field-level details to DuplicateEntryException
    - Update GlobalExceptionHandler for new error structure
    - Simplify SecurityExceptionHandler

--- common/src/main/java/com/fernirx/lms/common/exceptions/DuplicateEntryException.java
+++ common/src/main/java/com/fernirx/lms/common/exceptions/DuplicateEntryException.java
@@ -1,8 +1,20 @@
 package com.fernirx.lms.common.exceptions;
 
 import com.fernirx.lms.common.enums.ErrorCode;
+import com.fernirx.lms.common.utils.MessageFormatter;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 public class DuplicateEntryException extends LmsException{
+    private final String resourceName;
+    private final String fieldName;
+    private final Object fieldValue;
 
-    public DuplicateEntryException(ErrorCode errorCode)  { super(errorCode); }
+    public DuplicateEntryException(ErrorCode errorCode, String resourceName, String fieldName, Object fieldValue)  {
+        super(errorCode, MessageFormatter.duplicateEntry(resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
 }
--- common/src/main/java/com/fernirx/lms/common/exceptions/ResourceNotFoundException.java
+++ common/src/main/java/com/fernirx/lms/common/exceptions/ResourceNotFoundException.java
@@ -1,10 +1,26 @@
 package com.fernirx.lms.common.exceptions;
 
 import com.fernirx.lms.common.enums.ErrorCode;
+import com.fernirx.lms.common.utils.MessageFormatter;
+import lombok.Getter;
 
+@Getter
 public class ResourceNotFoundException extends LmsException {
+    private final String resourceName;
+    private final String fieldName;
+    private final Object fieldValue;
 
-    public ResourceNotFoundException(ErrorCode errorCode) {
-        super(errorCode);
+    public ResourceNotFoundException(ErrorCode errorCode,  String resourceName, String fieldName, Object fieldValue) {
+        super(errorCode, MessageFormatter.resourceNotFound(resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public ResourceNotFoundException(ErrorCode errorCode, String resourceName) {
+        super(errorCode, MessageFormatter.resourceNotFound(resourceName));
+        this.resourceName = resourceName;
+        this.fieldName = null;
+        this.fieldValue = null;
     }
 }
